2294499bf599fc670fef1bd31717a924cd3bc2be
tonyfontend <3513635208@qq.com>
2016-08-25 15:09:38 +0800

    Add files via upload

---
+++ map_post.js
@@ -0,0 +1,141 @@
+var post_map = [
+	/*css.html页面的文章开始*/
+	{
+		"title": "做一个略调皮的个人博客--菜单篇",
+		"catgory": "post css from",
+		"dest": "post\\css\\from\\2015-04-01-fromone-1",
+		"time": "2016-08-24",
+		"intro": "\r\n",
+		"author": "fontEndTony",
+		"model": "css-page"
+	},
+	{
+		"title": "grunt前端打包--css篇",
+		"catgory": "post css base",
+		"dest": "post\\css\\base\\2016-08-25-grunt-css",
+		"time": "2016-08-25",
+		"intro": "\r\n",
+		"author": "fontEndTony",
+		"model": "css-page"
+	}, {
+		"title": "像boot一样的去编程",
+		"catgory": "post css base",
+		"dest": "post\\css\\base\\2016-08-25-xiangbootyiyagnbiancheng",
+		"time": "2016-08-25",
+		"intro": "\r\n",
+		"author": "fontEndTony",
+		"model": "css-page"
+	}, 
+	{
+		"title": "网上常见的一个时钟~",
+		"catgory": "post css animation",
+		"dest": "post\\css\\animation\\2016-08-25-shizhongdemo",
+		"time": "2016-08-25",
+		"intro": "\r\n",
+		"author": "fontEndTony",
+		"model": "css-page"
+	}, 
+	{
+		"title": "扩展一个boot的插件——tooltip & 做一个基于boot的表达验证",
+		"catgory": "post css css3",
+		"dest": "post\\css\\css3\\2016-08-25-xuanzeqi",
+		"time": "2016-08-25",
+		"intro": "\r\n",
+		"author": "fontEndTony",
+		"model": "css-page"
+	},
+	/*css.html页面的文章结束*/
+	/*easy.html页面文章开始*/
+	{
+		"title": "杂记杂记",
+		"catgory": "post essay notes",
+		"dest": "post\\essay\\notes\\2016-08-22-zajizaji",
+		"time": "2016-08-22",
+		"intro": "\r\n你看，无论你从哪里窥视，都只能得到相同的答案。\r\n我想，就算全部从头来过，也只会换来相同的结局。\r\n轮回就是两场似是而非，谁也参不透其中的奥秘，舍得不过是一半坚韧，一半仁慈，没那么容易选择。你看，人世间那么多的蹉跎，无外乎轮回在舍与不舍。",
+		"author": "fontEndTony",
+		"model": "essay-page"
+	},
+	{
+		"title": "久违的杂记",
+		"catgory": "post essay notes",
+		"dest": "post\\essay\\notes\\2016-08-22-jiuweidezaji",
+		"time": "2016-08-22",
+		"intro": "\r\n早上在车站等公交车上班，迎面走来一个男子，左手拎了个类似果篮的东西，里面装满了大概是圣诞节用剩下的彩带。右手拿着一个好像公文包的东西，上面有两面掉了颜色的彩旗，还有三个不同颜色的干瘪的气球。他走向垃圾桶，首先看了看烟灰缸。翻找了半天，还是没有看到合适的烟头，于是又瞄了一眼垃圾桶的里面，也许也没有什么钟意的，又拎起自己的东西，沿着马路向前走着。走到一个街角，从兜里掏出了半支烟头，点着抽了起来。\r\n",
+		"author": "fontEndTony",
+		"model": "essay-page"
+	}, 
+	{
+		"title": "以沫",
+		"catgory": "post essay formerly",
+		"dest": "post\\essay\\formerly\\2016-08-25-yimo",
+		"time": "2016-08-25",
+		"intro": "\r\n传说鱼的记忆只有七秒1111111，你说，泉水干涸时，那两条互相润湿的鱼，是否能记得对方？\r\n一定记得。\r\n怎么如此确定？\r\n要不然怎么会有后面的一句，不如相忘于江湖呢，那一定是记得的。\r\n也许是不记得的，只是每次新的记忆都在重复从前，像是一世一世的轮回，都在追求相同的自己。\r\n我的想法是打破命运，你的说法，是在遵从命运。\r\n孰对孰错？\r\n无对无错。\r\n",
+		"author": "fontEndTony",
+		"model": "essay-page"
+	}, 
+	{
+		"title": "想不出题目",
+		"catgory": "post essay formerly",
+		"dest": "post\\essay\\formerly\\2016-08-09-notimu",
+		"time": "2018-08-09",
+		"intro": "\r\n今天高中班级聚会，由于晚上睡的太晚，十点多才起来，所以慌慌张张的洗漱穿衣服，出来的时候发现眼镜忘了带，我想算了吧，反正也不是去上网，没必要。\r\n我去的很晚，到的时候大部分都到了，男女分开，一共两桌，每桌一半多的人。我第一眼看到的就是她——即使没带眼镜，却还是那么真切——忙坐在背对的女生的位置，菜还没有上，桌子上连喝的都没有，双手根本不知道要做什么，忙问旁边要烟，一颗一颗的抽起来。\r\n",
+		"author": "fontEndTony",
+		"model": "essay-page"
+	}, 
+	{
+		"title": "随笔",
+		"catgory": "post essay young",
+		"dest": "post\\essay\\young\\2016-08-21-suibi",
+		"time": "2016-08-21",
+		"intro": "\r\n中秋节没有吃月饼，就像端午节也不喜欢吃粽子一样。我的家人同我一样没有过节的意识，我想归根结底是因为无论是粽子月饼还是元宵，我们都不爱吃。\r\n所以啤酒节或者烧烤节的话，我还是会去捧场的。\r\n今天在家看到电视上播了爱情公寓二，不得不说我还是很喜欢的。还是很逗乐的。抛却恋旧情节，张伟也傻的蛮可爱的，就算不抛却，秦羽墨也是很漂亮的···",
+		"author": "fontEndTony",
+		"model": "essay-page"
+	}, 
+	{
+		"title": "随笔",
+		"catgory": "post essay young",
+		"dest": "post\\essay\\young\\2016-08-20-suibi",
+		"time": "2016-08-20",
+		"intro": "\r\n关于那岛确实已经说不出什么了。先动一动手，把自己电脑里的片儿删干净，再出去打劫吧，起码这样，更加理直气壮一点。毕竟不管你有没有屎，蹲下去了，最先做的就应该是脱下裤子。\r\n我拿着索尼的本儿看着你用佳能照相机拍到的穿着美津浓的爷们儿咋着尼桑的车，到底谁更讽刺一点。\r\n昨儿个看了一组相册，里面有一句话很好，大概的意思是，昨天已经不可挽回，但是如果你执着于明天，就会失去今天。",
+		"author": "fontEndTony",
+		"model": "essay-page"
+	}, 
+	/*easy.html结束*/
+	/*phrase.html开始*/
+	{
+		"title": "梦呓",
+		"catgory": "post phrase prose",
+		"dest": "post\\phrase\\prose\\2016-07-22-mengyi",
+		"time": "2016-07-22",
+		"intro": "\r\n\r\n",
+		"author": "fontEndTony",
+		"model": "phrase-page"
+	}, 
+	{
+	"title": "若茗思雯",
+	"catgory": "post phrase prose",
+	"dest": "post\\phrase\\prose\\2016-07-28-ruomingsiwen",
+	"time": "2016-07-28",
+	"intro": "\r\n\r\n",
+	"author": "fontEndTony",
+	"model": "phrase-page"
+	},
+	{
+		"title": "以奏肤功",
+		"catgory": "post phrase novel",
+		"dest": "post\\phrase\\novel\\2016-07-03-yizoufugong",
+		"time": "2016-07-03",
+		"intro": "\r\n\r\n",
+		"author": "fontEndTony",
+		"model": "phrase-page"
+	}, 
+	{
+		"title": "木犹如此",
+		"catgory": "post phrase novel",
+		"dest": "post\\phrase\\novel\\2016-07-30-muyouruci",
+		"time": "2016-07-30",
+		"intro": "\r\n\r\n",
+		"author": "fontEndTony",
+		"model": "phrase-page"
+	}, 
+]
